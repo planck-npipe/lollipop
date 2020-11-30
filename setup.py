0d66c53d4efcf472f3c72d9e9346cd5c9da8890e
Xavier Garrido <xavier.garrido@gmail.com>
2020-11-30 20:13:58 +0100

    more complete setup.py + new actions to directly deploy to pypi

--- setup.py
+++ setup.py
@@ -2,12 +2,28 @@ from setuptools import find_packages, setup
 
 import versioneer
 
+with open("README.md") as f:
+    long_description = f.read()
+
 setup(
     name="planck_2020_lollipop",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     description="A cobaya low-ell likelihood polarized for planck",
-    zip_safe=True,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Matthieu Tristram",
+    url="https://github.com/planck-npipe/lollipop",
+    license="GNU license",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
     packages=find_packages(),
     python_requires=">=3.5",
     install_requires=["astropy", "cobaya>=3.0"],
